7b08f52b11fe85ae853d8a686cfed440c84af413
Venkat <32324848+rnibhanu@users.noreply.github.com>
2019-02-20 15:08:16 +0530

    Update App.java

--- src/main/java/com/testing/sample/App.java
+++ src/main/java/com/testing/sample/App.java
@@ -5,9 +5,9 @@ package com.testing.sample;
  *
  */
 public class App 
-{
-    public static void main( String[] args )
     {
+    public static void main( String[] args )
+     {
         System.out.println( "Hello World! Venkat" );
+     }
     }
-}
